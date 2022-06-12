408d6777a10a081eab8df0e723776cc551b2bbec
Kevin Riexinger <info@kevinriexinger.de>
2022-06-12 23:01:15 +0200

    Init

---
+++ prntsc.py
@@ -0,0 +1,13 @@
+from email.mime import base
+import random as rnd
+import string
+
+base_url = "https://prnt.sc/"
+
+x = 0
+
+while x < 10:
+    x = x + 1
+    y = 0
+    code = "".join(rnd.choice(string.ascii_lowercase + string.digits) for _ in range(6))
+    print(base_url + code)
